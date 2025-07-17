a3ed9f2db0e961651f450939a72f3c69ed1bbe1f
maryhiguera <marylin0higuera@gmail.com>
2025-07-17 17:46:22 -0600

    adding update, destroy

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -3,11 +3,13 @@ import axios from "axios";
 import JobsIndex from "./JobsIndex.vue";
 import JobsNew from "./JobsNew.vue";
 import Modal from "./Modal.vue";
+import JobsShow from "./JobsShow.vue"
 
 export default {
   components: {
     JobsIndex,
     JobsNew,
+    JobsShow,
     Modal,
   },
   data: function () {
@@ -22,13 +24,13 @@ export default {
   },
   methods: {
     handleIndexJobs: function () {
-      axios.get("http://localhost:5000/jobs.json").then((response) => {
+      axios.get("/jobs.json").then((response) => {
         console.log("jobs index", response);
         this.jobs = response.data;
       });
     },
     handleCreateJob: function (params) {
-      axios.post("http://localhost:5000/jobs.json", params).then((response) => {
+      axios.post("/jobs.json", params).then((response) => {
         console.log("jobs create", response);
         this.jobs.push(response.data);
       })
@@ -40,6 +42,31 @@ export default {
       console.log("handleShowJob", job);
       this.currentJob = job;
       this.isJobsShowVisible = true;
+    },
+    handleUpdateJob: function (id, params) {
+      console.log("handleUpdateJob", id, params);
+      axios.patch(`/jobs/${id}.json`).then((response) => {
+        console.log("jobs update", response);
+        this.jobs = this.jobs.map((job) => {
+          if (job.id === response.data.id) {
+            return response.data;
+          } else {
+            return job; 
+          }
+        });
+        this.handleClose();
+      })
+      .catch((error) => {
+        console.log("job update error", error.response);
+      });
+    },
+    handleDestroyJob: function (job) {
+      axios.delete(`/jobs/${id}.json`).then((response) => {
+        console.log("destroy job", response);
+        var index = this.jobs.indexOf(job);
+        this.jobs.splice(index, 1);
+        this.handleClose(); 
+      });
     },
     handleClose: function () {
       this.isJobsShowVisible = false;
@@ -53,7 +80,7 @@ export default {
     <JobsNew v-on:createJob="handleCreateJob" />
     <JobsIndex v-bind:jobs="jobs" v-on:showJob="handleShowJob"/>
     <Modal v-bind:show="isJobsShowVisible" v-on:close="handleClose" >
-      <h1>test</h1>
+      <JobsShow v-bind:job="currentJob" v-on:updateJob="handleUpdateJob" v-on:destroyJob="handleDestroyJob" />
     </Modal>
   </main>
 </template>
---
+++ src/components/JobsShow.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+  props: {
+    job: Object,
+  },
+  data: function () {
+    return {
+      editJobParams: {},
+    };
+  },
+  created: function () {
+    this.editJobParams = { ...this.job };
+  },
+  methods: {
+    handleSubmit: function () {
+      this.$emit("updateJob", this.job.id, this.editJobParams);
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>Job Information</h1>
+    <p>Title: {{ job.title }}</p>
+    <p>Company: {{ job.company }}</p>
+    <p>Description: {{ job.description }}</p>
+    <p>Salary: {{ job.salary }}</p>
+    <form v-on:submit.prevent="handleSubmit">
+      <div>
+        Title: <input v-model="editJobParams.title" type="text" />
+      </div>
+      <div>
+        Company: <input v-model="editJobParams.company" />
+      </div>
+      <div>
+        Description <input v-model="editJobParams.description" />
+      </div>
+      <div>
+        Salary: <input v-model="editJobParams.salary" />
+      </div>
+      <button type="submit">Update</button>
+      <button v-on:click="$emit('destroyJob', job)">Destroy</button>
+    </form>
+  </div>
+</template>
+
+<style></style>
